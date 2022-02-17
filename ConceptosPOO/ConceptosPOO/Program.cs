e460f1ba7c97f6040deb1aa1c3d6bc40480cfa4b
Alfredo <alfredosotero@hotmail.com>
2022-02-17 11:19:32 -0500

    finish date class

---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPOO;
+
+Console.WriteLine("Conceptos de POO");
+Console.WriteLine("================");
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 2, 7));
+    Console.WriteLine(new Date(2022, 11, 3));
+
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
