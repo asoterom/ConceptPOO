47a9eb02d021dede9218032eef6563f8210a2603
Alfredo <alfredosotero@hotmail.com>
2022-02-18 13:27:51 -0500

    Employee, SalaryEmployee, CommissionEmployee

---
+++ ConceptosPOO/ConceptosPOO/CommissionEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    public class CommissionEmployee : Employee
+    {
+        public float CommissionPercentage { get; set; }
+        public decimal Sales { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)CommissionPercentage;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\t Commission Percentage ........: {$"{CommissionPercentage:P2}",18} " +
+                $"\n\t Sales ........: {$"{Sales:C2}",18} " +
+                $"\n\t Value to pay ........: {$"{GetValueToPay():C2}",18} ";
+        }
+    }
+}
--- ConceptosPOO/ConceptosPOO/Program.cs
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -9,6 +9,30 @@ try
     Console.WriteLine(new Date(2022, 2, 7));
     Console.WriteLine(new Date(2022, 11, 3));
 
+    Employee employee1 = new SalaryEmployee()
+    {
+        id = 1010,
+        FirstName = "Sandra",
+        LastName = "morares",
+        BirthDate = new Date(1990, 5, 23),
+        HiringDate = new Date(2022, 1, 15),
+        IsActive = true,
+        Salary = 1815453.45M
+    };
+    Console.WriteLine(employee1);
+    Employee employee2 = new CommissionEmployee()
+    {
+        id = 2020,
+        FirstName = "Patricia",
+        LastName = "Gutierrez",
+        BirthDate = new Date(1995, 5, 23),
+        HiringDate = new Date(2022, 2, 5),
+        IsActive = true,
+        Sales = 320000000M,
+        CommissionPercentage = 0.03F
+    };
+    Console.WriteLine(employee2);
+
 }
 catch (Exception error)
 {
